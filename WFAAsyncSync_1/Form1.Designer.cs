c9d3eac08e7d8043001648dc6c3a38a18393575e
cagri.yolyapar <cagri.yolyapar@gmail.com>
2024-03-07 14:13:59 +0300

    Add project files.

---
+++ WFAAsyncSync_1/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WFAAsyncSync_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnSim = new Button();
+            btnSelam = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // btnSim
+            // 
+            btnSim.Location = new Point(12, 69);
+            btnSim.Name = "btnSim";
+            btnSim.Size = new Size(94, 29);
+            btnSim.TabIndex = 0;
+            btnSim.Text = "Simulasyon";
+            btnSim.UseVisualStyleBackColor = true;
+            btnSim.Click += btnSim_Click;
+            // 
+            // btnSelam
+            // 
+            btnSelam.Location = new Point(306, 69);
+            btnSelam.Name = "btnSelam";
+            btnSelam.Size = new Size(94, 29);
+            btnSelam.TabIndex = 1;
+            btnSelam.Text = "Selamla";
+            btnSelam.UseVisualStyleBackColor = true;
+            btnSelam.Click += btnSelam_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(107, 210);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(btnSelam);
+            Controls.Add(btnSim);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnSim;
+        private Button btnSelam;
+        private TextBox textBox1;
+    }
+}
---
+++ WFAAsyncSync_1/Form1.cs
@@ -0,0 +1,50 @@
+﻿namespace WFAAsyncSync_1
+{
+    public partial class Form1 : Form
+    {
+        //Bir metodun asenkron calısabilmesini istiyorsanız o metot geriye deger döndürse de döndürmese de önce Task tipi ile yaratılmalıdır...Task tipi  yaratıldıktan sonra metot eger geriye deger döndürmeyecekse Task'e baska bir sey yazılmaz...Eger geriye deger döndürecekse o deger tipi Task'e generic olarak verilir
+
+
+        Task SelamlaAsync()
+        {
+            return Task.Run(() =>
+            {
+
+                Thread.Sleep(10000);
+                MessageBox.Show("Async olarak cagrıldıgım icin görevim kimseyi engellemedi");
+
+            });
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int KareAl(int sayi)
+        {
+            Thread.Sleep(10000);
+            return sayi * sayi;
+        }
+
+        //await keyword'u kullanılacak ise ilgili metodun cagrıldıgı alanın async keyword'une sahip olması gerekir...
+        private async void btnSim_Click(object sender, EventArgs e)
+        {
+            await SelamlaAsync();
+            MessageBox.Show("Gorev bitti");
+        }
+
+        private void btnSelam_Click(object sender, EventArgs e)
+        {
+            Selamla();
+        }
+
+
+        public void Selamla()
+        {
+            Thread.Sleep(10000);
+            MessageBox.Show("Hello World");
+        }
+    }
+}
